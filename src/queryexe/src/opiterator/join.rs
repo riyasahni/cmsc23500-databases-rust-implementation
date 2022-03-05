87c39c6deb70d7b9e5eb6eeef7894f60a69824e6
riyasahni <sahnir@uchicago.edu>
2022-03-05 16:48:44 -0600

    finished q0. moving onto perf

--- src/queryexe/src/opiterator/join.rs
+++ src/queryexe/src/opiterator/join.rs
@@ -1,5 +1,6 @@
 use super::{OpIterator, TupleIterator};
 use common::{CrustyError, Field, SimplePredicateOp, TableSchema, Tuple};
+use sqlparser::dialect::keywords::NO;
 use std::collections::HashMap;
 
 /// Compares the fields of two tuples using a predicate. (You can add any other fields that you think are neccessary)
@@ -36,12 +37,20 @@ pub struct Join {
     predicate: JoinPredicate,
     /// Left child node.
     left_child: Box<dyn OpIterator>,
+    /// Left index
+    left_index: usize,
     /// Right child node.
     right_child: Box<dyn OpIterator>,
+    /// Right index
+    right_index: usize,
     /// Schema of the result.
     schema: TableSchema,
     /// Bool to indicate open or closed Join
     is_open: bool,
+    /// Bool keeps track of beginning of iteration
+    is_beg: bool,
+    /// save where left child is at
+    lc_saved: Option<Tuple>,
 }
 
 impl Join {
@@ -75,9 +84,13 @@ impl Join {
         let new_join = Join {
             predicate: join_predicate,
             left_child,
+            left_index,
             right_child,
+            right_index,
             schema: new_schema,
             is_open: false,
+            is_beg: true,
+            lc_saved: Some(Tuple::new(Vec::new())),
         };
         // return new join struct
         new_join
@@ -97,10 +110,69 @@ impl OpIterator for Join {
 
     /// Calculates the next tuple for a nested loop join.
     fn next(&mut self) -> Result<Option<Tuple>, CrustyError> {
-        /*for t_r in self.right_child.next().iter() {
-            for t_l in self.left_child.next().iter() {}
-        }*/
-        panic!("TODO milestone op");
+        if !self.is_open {
+            panic!("Open OpIterator!")
+        }
+        //  let mut lt = self.left_child.next().unwrap();
+        let mut rt = self.right_child.next().unwrap();
+
+        if self.is_beg {
+            println!("initialize left child's saved val");
+            self.lc_saved = self.left_child.next().unwrap().clone();
+            self.is_beg = false;
+        }
+
+        match rt {
+            Some(r_tup) => {
+                println!("Some right child next exists");
+                let l = self.lc_saved.clone();
+                match l {
+                    Some(l) => {
+                        println!("Some right child next exists");
+                        // check tuple's predicate and see if tuple should be joined or not
+                        let r_field = Tuple::get_field(&r_tup, self.right_index).unwrap();
+                        let l_field = Tuple::get_field(&l, self.left_index).unwrap();
+                        let check = self.predicate.op.compare(&l_field, &r_field);
+
+                        if check {
+                            let tup = l.merge(&r_tup);
+                            // self.lc_saved = Some(l);
+                            return Ok(Some(tup));
+                        } else {
+                            println!("getting here");
+                            self.next()
+                        }
+                    }
+                    None => return Ok(None),
+                }
+            }
+            None => {
+                self.right_child.rewind()?;
+                rt = self.right_child.next().unwrap();
+                let lt = self.left_child.next().unwrap();
+                match lt {
+                    Some(ref l) => {
+                        // check tuple's predicate and see if tuple should be joined or not
+                        let r_field =
+                            Tuple::get_field(&rt.as_ref().unwrap(), self.right_index).unwrap();
+                        let l_field = Tuple::get_field(&l, self.left_index).unwrap();
+                        let check = self.predicate.op.compare(&l_field, &r_field);
+
+                        if check {
+                            let tup = l.merge(&rt.unwrap());
+                            self.lc_saved = lt;
+                            return Ok(Some(tup));
+                        } else {
+                            self.lc_saved = lt;
+                            self.next()
+                        }
+                    }
+                    None => {
+                        return Ok(None);
+                    }
+                }
+            }
+        }
     }
 
     fn close(&mut self) -> Result<(), CrustyError> {
@@ -112,14 +184,19 @@ impl OpIterator for Join {
         self.right_child.close()?;
         self.left_child.close()?;
         self.is_open = false;
-        // close join
-        // self.close();
         Ok(())
     }
 
     fn rewind(&mut self) -> Result<(), CrustyError> {
+        if !self.is_open {
+            panic!("Open the iterator!");
+        }
+        self.right_child.rewind()?;
+        self.left_child.rewind()?;
+        self.close()?;
+        self.open();
+        Ok(())
         // reopen the iterator?
-        panic!("TODO milestone op");
     }
 
     /// return schema of the result
@@ -131,13 +208,20 @@ impl OpIterator for Join {
 /// Hash equi-join implementation. (You can add any other fields that you think are neccessary)
 pub struct HashEqJoin {
     predicate: JoinPredicate,
-
+    left_index: usize,
+    right_index: usize,
     left_child: Box<dyn OpIterator>,
     right_child: Box<dyn OpIterator>,
-
+    /// add element to store val of right child btw iterations
+    saved_right: Option<Tuple>,
+    right_stay: bool,
     schema: TableSchema,
+    open: bool,
+    /// need to store 1 child's info in hashmap
+    join_hashmap: HashMap<Field, HashMap<usize, Tuple>>,
+    /// keep track of explored values in hashmap
+    hashmap_index: usize,
 }
-
 impl HashEqJoin {
     /// Constructor for a hash equi-join operator.
     ///
@@ -156,27 +240,111 @@ impl HashEqJoin {
         left_child: Box<dyn OpIterator>,
         right_child: Box<dyn OpIterator>,
     ) -> Self {
-        panic!("TODO milestone op");
+        let join_predicate = JoinPredicate::new(op, left_index, right_index);
+        let left_schema = left_child.get_schema().clone();
+        let right_schema = right_child.get_schema().clone();
+        // schema is just left and right merged
+        let joined_schema = left_schema.merge(&right_schema);
+        let new_hasheq_join = HashEqJoin {
+            predicate: join_predicate,
+            left_index,
+            right_index,
+            left_child,
+            right_child,
+            saved_right: None,
+            right_stay: false,
+            schema: joined_schema,
+            open: false,
+            join_hashmap: HashMap::new(),
+            hashmap_index: 0,
+        };
+        return new_hasheq_join;
     }
 }
-
 impl OpIterator for HashEqJoin {
     fn open(&mut self) -> Result<(), CrustyError> {
-        panic!("TODO milestone op");
+        self.left_child.open();
+        self.right_child.open();
+        self.open = true;
+        // populate hash map for left child
+        while let Some(l) = self.left_child.next().unwrap() {
+            let key = l.get_field(self.left_index).unwrap();
+            // case where key in outer hashmap
+            if self.join_hashmap.contains_key(&key) {
+                let inner_hashmap = self.join_hashmap.get_mut(&key).unwrap();
+                let len_inner_hashmap = inner_hashmap.keys().len();
+                inner_hashmap.insert((len_inner_hashmap - 1), l.clone());
+            }
+            // if not, add key, val pair to outer hashmap
+            else {
+                let mut new_inner_hashmap = HashMap::new();
+                new_inner_hashmap.insert(0, l.clone());
+                self.join_hashmap.insert(key.clone(), new_inner_hashmap);
+            }
+        }
+        Ok(())
     }
-
     fn next(&mut self) -> Result<Option<Tuple>, CrustyError> {
-        panic!("TODO milestone op");
+        if !self.open {
+            panic!("Operator has not been opened")
+        }
+        // set right as either stored or next
+        let mut right_val = self.saved_right.clone();
+        let mut right_val_new = Tuple::new(Vec::new());
+        if !self.right_stay {
+            self.right_stay = true;
+            right_val = self.right_child.next().unwrap();
+            match right_val {
+                Some(x) => right_val_new = x,
+                None => return Ok(None),
+            };
+            self.saved_right = Some(right_val_new.clone());
+        }
+        /// search hashmap for matches, use hashmap index to deal with multiple values / key  
+        let right_field = right_val_new.get_field(self.right_index);
+        match right_field {
+            Some(rf) => {
+                if self.join_hashmap.contains_key(&rf) {
+                    let inner_hashmap = self.join_hashmap[&rf].clone();
+                    let left_val = &inner_hashmap[&self.hashmap_index];
+                    let merged_val = left_val.merge(&right_val_new);
+                    self.hashmap_index += 1;
+                    if self.hashmap_index == inner_hashmap.keys().len() {
+                        self.hashmap_index = 0;
+                        self.right_stay = false;
+                    } else {
+                        self.hashmap_index += 1;
+                        self.right_stay = true;
+                    }
+                    return Ok(Some(merged_val));
+                } else {
+                    self.hashmap_index = 0;
+                    self.right_stay = false;
+                    return self.next();
+                }
+            }
+            None => return Ok(None),
+        }
     }
-
     fn close(&mut self) -> Result<(), CrustyError> {
-        panic!("TODO milestone op");
+        if !self.open {
+            panic!("Operator has not been opened")
+        }
+        self.left_child.close()?;
+        self.right_child.close()?;
+        self.open = false;
+        Ok(())
     }
-
     fn rewind(&mut self) -> Result<(), CrustyError> {
-        panic!("TODO milestone op");
+        if !self.open {
+            panic!("Operator has not been opened")
+        }
+        self.left_child.rewind()?;
+        self.right_child.rewind()?;
+        self.right_stay = false;
+        self.close()?;
+        self.open()
     }
-
     fn get_schema(&self) -> &TableSchema {
         &self.schema
     }
